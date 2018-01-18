e34c678fa79bb0c2e3158fdc2bed977a36113b4e
cpandey <cpandey@localhost.localdomain>
2018-01-18 19:06:06 +0530

    get username from userprincipal class.

--- mbeanExposeTest/ReadMe.txt
+++ mbeanExposeTest/ReadMe.txt
@@ -1,6 +1,11 @@
 
 A simple example where operation is invoked as mbean.
+Also it shows how we can get username within the operation.
 
     osgi:install -s mvn:mbean-expose/mbeanExposeTest/0.0.1-SNAPSHOT
+    
+    one can test this using
+    
+    curl  -u  admin:admin http://localhost:8181/hawtio/jolokia/exec/com.test.mbean:type=beans,name=exposeMbeanTest/hello
 
 
--- mbeanExposeTest/src/main/java/com/test/mbean/BaseMBean.java
+++ mbeanExposeTest/src/main/java/com/test/mbean/BaseMBean.java
@@ -1,8 +1,14 @@
 package com.test.mbean;
 
 
+import java.security.AccessControlContext;
+import java.security.AccessController;
+import java.util.Set;
+
 import javax.management.NotCompliantMBeanException;
 import javax.management.StandardMBean;
+import javax.security.auth.Subject;
+import org.apache.karaf.jaas.boot.principal.UserPrincipal;
 
 public class BaseMBean extends StandardMBean implements Hello {
 
@@ -12,6 +18,18 @@ public class BaseMBean extends StandardMBean implements Hello {
 
 	@Override
 	public String hello() {
+		
+		AccessControlContext acc = AccessController.getContext();
+	    Subject subject = Subject.getSubject(acc);
+	    if (subject != null)  {
+	    Set<UserPrincipal> principals = subject.getPrincipals(UserPrincipal.class);
+	    try{
+	    UserPrincipal principal = principals.iterator().next();
+	    System.out.println("Username: "+principal.getName());
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+	    }
 		return "Perfect operation is invoked as mbean";
 	}
 }
